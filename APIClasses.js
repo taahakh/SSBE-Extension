f65c93ac72f0ca95d68b7e731bbd6fd9d37c7596
Ahnaf Khalique <ahnafkhalique@hotmail.com>
2024-02-15 00:05:28 +0000

    Some more progress

---
+++ APIClasses.js
@@ -0,0 +1,9 @@
+class SummariserClass {
+    constructor() {
+        // this.summariser = new Summariser();
+    }
+    summarise(text) {
+        // return this.summariser.summarise(text);
+        return text;
+    }
+}
--- popup.js
+++ popup.js
@@ -9,6 +9,8 @@ const postF = document.getElementById('postButton');
 
 postF.addEventListener('click', async () => {
   console.log('Starting POST Request process');
+  // let x = new SummariserClass();
+  // console.log(x.summarise("Hello World"));
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "gatherData", usingXpath: false});
     });
